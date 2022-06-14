c85820443513b4c890f52c64c3fd41162d49220e
Anatoliy Lut <51475542+zerotoolz@users.noreply.github.com>
2022-06-14 10:44:59 +0800

    main

---
+++ test.py
@@ -0,0 +1,14 @@
+import agorartc
+rtc = agorartc.createRtcEngineBridge()
+eventHandler = agorartc.RtcEngineEventHandlerBase()
+rtc.initEventHandler(eventHandler)
+0 (Success)
+rtc.initialize("f3d54112e0b243599ff58abcbd4ed38c", None, agorartc.AREA_CODE_GLOB & 0xFFFFFFFF)  # If you do not have an App ID, see Appendix (https://github.com/AgoraIO-Community/Agora-Python-SDK#appendix).
+0 (Success)
+rtc.enableVideo()
+0 (Success)
+rtc.joinChannel("", "channel-name", "", 0)
+0 (Success)
+rtc.leaveChannel()  # Leave channel
+0 (Success)
+rtc.release()
